ffa4d47dbbe30ec0949c7e9f24773bff97809dbe
Jannis Lübke <jannis.luebke@campus.tu-berlin.de>
2026-06-07 15:22:35 +0200

    feat(orchestrator): manifest_quality evaluator

--- packages/orchestrator/src/aidmi_orchestrator/evaluator/__init__.py
+++ packages/orchestrator/src/aidmi_orchestrator/evaluator/__init__.py
@@ -4,3 +4,4 @@ from aidmi_orchestrator.evaluator import execution  # noqa: F401
 from aidmi_orchestrator.evaluator import llm_usage  # noqa: F401
 from aidmi_orchestrator.evaluator import schema  # noqa: F401
 from aidmi_orchestrator.evaluator import row_equality  # noqa: F401
+from aidmi_orchestrator.evaluator import manifest_quality  # noqa: F401
